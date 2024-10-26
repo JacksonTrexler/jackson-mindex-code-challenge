abbbbe80dfcc7e93a8b9e637c4da2ca027cc442e
mindex <dev@mindex.com>
2024-10-26 03:50:58 -0400

    initial task 1 complete

--- src/main/java/com/mindex/challenge/service/impl/ReportingStructureImpl.java
+++ src/main/java/com/mindex/challenge/service/impl/ReportingStructureImpl.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -20,9 +21,35 @@ public class ReportingStructureImpl implements ReportingStructureService {
 
     @Override
     public ReportingStructure generateReportingStructure(String id) {
+        //Set employee and base ReportingStructure
+        int count = 0;
+        List<Employee> richReports = new ArrayList<>();
         Employee employee = employeeRepository.findByEmployeeId(id);
-        LOG.debug("Retrieving reporting structure from employee [{}]", employee.getFirstName()); //todo test null name
-        return new ReportingStructure(employee, countReports(employee));
+
+        //This is a little ugly, but keeps this recursive method a bit more self-contained
+        if (employee == null){
+            return null;
+        }
+
+        //If the employee doesn't have any reports, just return a structure with the full employee
+        //To prevent NPE exceptions and unneeded code execution if an employee doesn't have reports
+        if (employee.getDirectReports() == null || employee.getDirectReports().isEmpty()){
+            return new ReportingStructure(employee, 0);
+        }
+
+        //For each report, create another report structure, count them as well as their reports, and update this
+        //Employee's reports to have full information
+        for (Employee report : employee.getDirectReports()){
+            ReportingStructure reportStructure = generateReportingStructure(report.getEmployeeId());
+            if (reportStructure == null){
+                continue;
+            }
+            count += 1 + reportStructure.getNumberofReports();
+            richReports.add(reportStructure.getEmployee());
+        }
+        employee.setDirectReports(richReports);
+        //LOG.debug("Retrieving reporting structure from employee [{}]", employee.getFirstName()); //todo test null name
+        return new ReportingStructure(employee, count);
     }
 
     private int countReports(Employee employee) {
@@ -45,5 +72,21 @@ public class ReportingStructureImpl implements ReportingStructureService {
         }
         System.out.println(count);
         return count;
+    }
+
+    private ReportingStructure populateReports(Employee employee){
+        int count = 0;
+        List<Employee> directReports = employee.getDirectReports();
+
+        if (directReports != null && !directReports.isEmpty()) {
+            for (Employee report : directReports) {
+                report = employeeRepository.findByEmployeeId(report.getEmployeeId());
+                count++;
+                if (report != null) {
+
+                }
+            }
+        }
+        return null;
     }
 }
--- src/test/java/com/mindex/challenge/ChallengeApplicationTests.java
+++ src/test/java/com/mindex/challenge/ChallengeApplicationTests.java
@@ -1,8 +1,12 @@
 package com.mindex.challenge;
 
+import com.mindex.challenge.service.EmployeeService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -12,5 +16,12 @@ public class ChallengeApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
+
+	@Autowired
+	private EmployeeService employeeService;
+
+	@LocalServerPort
+	private int port;
+
 
 }
